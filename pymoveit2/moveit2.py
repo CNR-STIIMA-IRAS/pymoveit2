f8f4331be2ec0381a911c1bc25dce2df4a77e056
Samuele Sandrini <samuele.sandrini@polito.it>
2025-08-07 18:54:40 +0200

    Fix blocking update planning scene

--- pymoveit2/moveit2.py
+++ pymoveit2/moveit2.py
@@ -1820,9 +1820,20 @@ class MoveIt2:
                 f"Service '{self._get_planning_scene_service.srv_name}' is not yet available. Better luck next time!"
             )
             return False
-        self.__planning_scene = self._get_planning_scene_service.call(
-            GetPlanningScene.Request()
-        ).scene
+
+        planning_scene_future = self._get_planning_scene_service.call_async(GetPlanningScene.Request())
+        time_now = self._node.get_clock().now().to_msg()
+        while not planning_scene_future.done():
+            if planning_scene_future.done():
+                break
+            if self._node.get_clock().now().to_msg().sec - time_now.sec > 3.0:
+                self._node.get_logger().warn(
+                    "Timeout while waiting for planning scene service call."
+                )
+                return False
+            rclpy.spin_once(self._node, timeout_sec=0.1)
+        self.__planning_scene = planning_scene_future.result().scene
+ 
         return True
 
     def allow_collisions(self, id: str, allow: bool) -> Optional[Future]:
